506807ed2d64157859f6779d29775dc78f3517e3
stolenhead <andrea.palomino.ruby@gmail.com>
2017-06-06 11:13:11 -0500

    ngfor li collapse

--- src/app/common/collapse/collapse.component.ts
+++ src/app/common/collapse/collapse.component.ts
@@ -1,12 +1,31 @@
 import { Component } from '@angular/core';
 
+const ITEMS: Item[] = [
+	{info: 'Mis Hijos', description: '(3 Personas)'},
+	{info: 'Mis Familiares', description: '(1 Persona)'},
+	{info: 'Personas que Apodero', description: '(1 Personas)'}
+];
+
 @Component ({
 	selector:  'collapse',
 	templateUrl: './collapse.component.html',
 	styleUrls: [
 		'./collapse.component.css'
-	]
+	],
 })
 
 export class CollapseComponent{
+	// items:Array<string> = ["Mis Hijos", "Mis Familiares", "Personas que Apodero"];
+	// number:Array<string> = ["3 personas", "1 Persona", "1 Persona"];
+	items = ITEMS;
+	itemSeleccionado = Item;
+
+	seleccionar(item){
+		this.itemSeleccionado = item;
+	}
+};
+
+export class Item{
+	info: string;
+	description: string;
 }
